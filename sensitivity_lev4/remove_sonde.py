a5ee720bf3a0877418979a9ce6d56b5d43404e27
hgloeckner <helene.gloeckner@posteo.de>
2025-02-17 13:45:04 +0100

    test and plotting remove sonde

---
+++ sensitivity_lev4/remove_sonde.py
@@ -0,0 +1,150 @@
+# %%
+import configparser
+
+import copy
+import numpy as np
+import pydropsonde.pipeline
+import xarray as xr
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pydropsonde.processor import Gridded
+import pydropsonde
+
+from helper_products import calc_products, remove_one, iterate_circle
+from helper_products import get_good, keep_good
+
+
+config = configparser.ConfigParser()
+config.read("../orcestra_drop.cfg")
+
+# %%
+root = "ipns://latest.orcestra-campaign.org/"
+l3_ds = xr.open_dataset(
+    f"{root}/products/HALO/dropsondes/Level_3/PERCUSION_Level_3.zarr", engine="zarr"
+).rename({"interp_time": "bin_average_time"})
+
+gridded = Gridded(sondes={}, global_attrs={})
+gridded.set_l3_ds(l3_ds.where((l3_ds["u_qc"] == 0) & (l3_ds["p_qc"] == 0), drop=True))
+gridded.get_circle_times_from_segmentation(
+    "https://orcestra-campaign.github.io/flight_segmentation/all_flights.yaml"
+)
+gridded.alt_dim = "altitude"
+gridded.sonde_dim = "sonde"
+# %%
+
+
+circles = pydropsonde.pipeline.create_and_populate_circle_object(gridded, None).circles
+
+good_circles = get_good(circles, thres=12)
+
+
+circles = keep_good(circles, good_circles)
+circles_play = copy.deepcopy(circles)
+
+# %%
+no_int_ref = iterate_circle(circles=circles_play, config=config, int=False)
+int_ref = iterate_circle(circles=circles_play, config=config, int=True)
+
+
+# %%
+result = {
+    "int": {key: [] for key in circles.keys()},
+    "no_int": {key: [] for key in circles.keys()},
+}
+sonde_ids = range(13)
+
+for gap_sonde in sonde_ids:
+    try:
+        gap_no_int = remove_one(
+            circles=circles_play, config=config, gap_sonde=gap_sonde, int=False
+        )
+    except IndexError:
+        pass
+    else:
+        gap_no_int = calc_products(gap_no_int, config)
+        gap_int = remove_one(
+            circles=circles_play, config=config, gap_sonde=gap_sonde, int=True
+        )
+        gap_int = calc_products(gap_int, config)
+        for key in gap_int.keys():
+            result["int"][key].append(gap_int[key].circle_ds)
+            result["no_int"][key].append(gap_no_int[key].circle_ds)
+
+
+# %%
+
+var = "omega"
+mean_err_c = {
+    "int": {key: [] for key in circles.keys()},
+    "no_int": {key: [] for key in circles.keys()},
+}
+for key in circles.keys():
+    cint_ref = int_ref[key].circle_ds
+    cno_int_ref = no_int_ref[key].circle_ds
+    for ds_int, ds_no_int in zip(result["int"][key], result["no_int"][key]):
+        mean_err_c["int"][key].append(
+            (ds_int[var] - cint_ref[var]).mean("altitude").values
+        )
+        mean_err_c["no_int"][key].append(
+            (ds_no_int[var] - cno_int_ref[var]).mean("altitude").values
+        )
+# %%
+colors = sns.color_palette("turbo", 22)
+fig, ax = plt.subplots()
+fig.suptitle("Remove one sonde")
+for idx, key in enumerate(mean_err_c["int"].keys()):
+    print("int", key, np.mean(mean_err_c["int"][key]))
+    print("no_int", key, np.mean(mean_err_c["no_int"][key]))
+    ax.scatter(
+        mean_err_c["int"][key],
+        mean_err_c["no_int"][key],
+        color=colors[idx],
+        s=10,
+        label=key,
+    )
+# ax.legend(loc=1, fontsize="small")
+ax.set_xlabel("interpolated mean error / hPa hr-1")
+ax.set_ylabel("not interpolated mean error / hPa hr-1")
+ax.axvline(0, color="gray", alpha=0.5, linestyle="-")
+ax.axhline(0, color="gray", alpha=0.5, linestyle="-")
+x = np.linspace(-4, 4)
+ax.fill_betweenx(x, -x, x, color="lightgray", alpha=0.2)
+sns.despine(offset=10)
+fig.tight_layout()
+fig.savefig("../images/remove_one_sonde.pdf")
+
+
+# %%
+
+int_err = np.concat(list(mean_err_c["int"].values()))
+no_int_err = np.concat(list(mean_err_c["no_int"].values()))
+
+fig, ax = plt.subplots()
+
+ax.hist(int_err, bins=50, histtype="bar", alpha=0.5, label="int", color="#00267f")
+ax.hist(no_int_err, bins=50, histtype="bar", alpha=0.5, label="no_int", color="#ffc726")
+ax.legend()
+ax.set_xlabel("mean error / hPa hr-1")
+ax.set_ylabel("count")
+ax.axvline(0, color="gray", alpha=0.5, linestyle="-")
+
+sns.despine(offset=10)
+fig.savefig("../images/hist_remove_one_sonde.png")
+# %%
+key = "HALO-20240816a_8c12"
+var = "omega"
+
+no_int_ds = no_int_ref[key].circle_ds
+int_ds = int_ref[key].circle_ds
+fig, ax = plt.subplots()
+fig.suptitle(key)
+ax.scatter(no_int_ds[var], no_int_ds.altitude, s=10, label="no int", c="#00267f")
+
+ax.scatter(int_ds[var], int_ds.altitude, s=10, label="int", c="#ffc726")
+
+ax.set_ylabel("gpsalt / m")
+ax.set_xlabel("omega / hPa hr-1")
+ax.legend()
+sns.despine(offset=10)
+fig.tight_layout()
+fig.savefig("../images/example_remove_one_sonde.png")
