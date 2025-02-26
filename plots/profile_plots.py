39835338d40ca288965ee298dd61f7ab31464281
hgloeckner <helene.gloeckner@posteo.de>
2025-02-26 18:09:54 +0100

    profile plots

---
+++ plots/profile_plots.py
@@ -0,0 +1,46 @@
+# %%
+
+import xarray as xr
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+lev3 = xr.open_dataset(
+    "/Users/helene/Documents/Data/Dropsonde/dropsonde_data/products/Level_3_qc/PERCUSION_Level_3.zarr"
+)
+# %%
+
+sal = lev3.where(lev3.aircraft_longitude > -40, drop=True)
+bb = lev3.where(lev3.aircraft_longitude < -40, drop=True)
+
+# %%
+
+csal = "#960018"
+cbb = "#0085db"
+variables = ["theta", "q", "u", "v"]
+
+
+fig, axes = plt.subplots(ncols=len(variables), figsize=(6 * len(variables), 6))
+
+for j, var in enumerate(variables):
+    for i in range(max([sal.sonde.size, bb.sonde.size])):
+        sonde = max([sal.sonde.size, bb.sonde.size]) - i - 1
+        try:
+            sal.sel(sonde=sonde)[var].plot(
+                ax=axes[j], color=csal, alpha=0.05, y="altitude"
+            )
+        except IndexError:
+            pass
+        bb.sel(sonde=sonde)[var].plot(ax=axes[j], color=cbb, alpha=0.05, y="altitude")
+
+    sal[var].mean("sonde").plot(ax=axes[j], color=csal, y="altitude", linewidth=5)
+    bb[var].mean("sonde").plot(ax=axes[j], color=cbb, y="altitude", linewidth=5)
+
+sns.despine(offset=10)
+
+for ax in axes:
+    ax.set_ylim(0, None)
+fig.tight_layout()
+fig.savefig("../images/profile_overview.png")
+
+# %%
