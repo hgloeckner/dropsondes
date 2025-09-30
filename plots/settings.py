517d1092bd8b6792f5246212cca1b20e3e79f6fe
hgloeckner <helene.gloeckner@posteo.de>
2025-09-30 18:07:35 +0200

    update cid

--- plots/settings.py
+++ plots/settings.py
@@ -10,7 +10,7 @@ colors = {
     "cbb_mean": "#00b4d8",
 }
 
-root = "ipfs://QmTpimQBT8AngwDPRTWYqCXctbhqMiP3NiPeEfXt3JomuU"
+root = "ipfs://QmRuaDianH2jryvZoehSES2nkqwLrEsbnwgYnRDLsM2cHZ"
 
 east_region = [[-34, 3], [-20, 3], [-20, 14], [-34, 14]]
 north_region = [[-26, 14], [-20, 14], [-20, 18], [-26, 18]]
